ccc6b1f3d73af37b43e6747b16590d94ba761f83
julietoge <julietogechi27@gmail.com>
2025-07-25 11:17:53 +0100

    update url.py & views.py

--- cs50w/lecture3/hello/views.py
+++ cs50w/lecture3/hello/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
